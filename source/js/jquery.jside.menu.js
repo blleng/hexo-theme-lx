e58cad22b966aa6f2448c117768e3ec6fb5d2c20
blleng <l0bl@outlook.com>
2020-03-09 17:16:13 +0800

    menu button

--- source/js/jquery.jside.menu.js
+++ source/js/jquery.jside.menu.js
@@ -1,16 +1,7 @@
-/*!  Plugin: jSide Menu (Responsive Side Menu) 
- *   Dependency: jQuery 3.4.1 & Material Design Iconic Font 2.0
- *   Author: Asif Mughal
- *   GitHub: https://github.com/CodeHimBlog
- *   URL: https://www.codehim.com
- *   License: MIT License
- *   Copyright (c) 2018 - 2019 - Asif Mughal
- */
-/* File: jquery.jside.menu.js */
 (function($){
      $.fn.jSideMenu = function(options){
     var setting = $.extend({
-        jSidePosition: "position-left", //possible options position-left or position-right 
+        jSidePosition: "position-right", //possible options position-left or position-right 
         jSideSticky: true, // menubar will be fixed on top, false to set static
         jSideSkin: "greenish", // to apply custom skin, just put its name in this string
         jSideTransition: 400, //Define the transition duration in milliseconds 
@@ -23,7 +14,7 @@
   devHeight = $(window).height();
   headHeight = $(".menu-head").height();
   dHeading = $(".dropdown-heading");
-  menuTrigger = $(".menu-trigger");
+  menuTrigger = $(".hamburger");
   arrow = $("<i></i>");
   dimBackground = $("<div>");
 // Set the height of side menu according to the available height of device
@@ -37,11 +28,9 @@ $(target).css({
    }
 $(".menubar").addClass(setting.jSideSkin);
 $(jSide).addClass(setting.jSideSkin).addClass(setting.jSidePosition);
-$(".menu-trigger").addClass("right");
+$(".hamburger").addClass("right");
 //Dropdown Arrow
     $(arrow).addClass("fa fa-chevron-down").appendTo(dHeading);
-//Dim Layer	
-    $(dimBackground).addClass("dim-overlay").appendTo("body");
 //Dropdowns
 $(dHeading).click(function(){
    $(this).parent().find("ul").slideToggle(setting.jSideTransition);
@@ -51,10 +40,10 @@ $(dHeading).click(function(){
 $(menuTrigger).click(function(){
    $(jSide).toggleClass("open");
    $(dimBackground).show(setting.jSideTransition);
+   $(".hamburger").toggleClass("is-active");
 });
-//close menu if user click outside of it
    $(window).click(function(e) {
-    if ($(e.target).closest(".menu-trigger").length){
+    if ($(e.target).closest(".hamburger").length){
      return;}
     if ($(e.target).closest(jSide).length){
      return;}
@@ -62,14 +51,8 @@ $(menuTrigger).click(function(){
     if (!$(jSide).hasClass("open")) {
      $(dimBackground).hide(setting.jSideTransition);
         }
+      $(".hamburger").removeClass("is-active");
       });
    });
  };
 })(jQuery);
-/*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
