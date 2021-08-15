39b131dd77d0e484c20176fff31e290474321ae1
blleng <l0bl@outlook.com>
2021-08-15 19:35:20 +0800

    JPEG Minified😊

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'),
     stylus = require("gulp-stylus"),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    imagemin = require("gulp-imagemin");
 
-function stylusToCss(){
+function stylusToMinCss(){
   gulp.src('source/css/main.styl')
     .pipe(rename({
       suffix: '.min'
@@ -12,10 +13,31 @@ function stylusToCss(){
     }))
     .pipe(gulp.dest('source/css'));
  };
+function imageMinify(){
+  gulp.src('source/images/*.jpeg*')
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality:70, progressive: true})
+    ]))
+    .pipe(gulp.dest('source/images'));
+};
 
-function build(cb) {
-  stylusToCss();
+function buildAll(cb){
+  stylusToMinCss();
+  imageMinify();
   cb();
- }
+}
+function MinImg(cb){
+  imageMinify();
+  cb();
+}
+function MinCss(cb){
+  stylusToMinCss();
+  cb();
+}
 
-exports.default = gulp.series(build);
+exports.default = buildAll;
+exports.img = MinImg;
+exports.css = MinCss;
