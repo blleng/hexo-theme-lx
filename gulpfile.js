590a6723c54b8683047de546fb16b6eb982942d2
blleng <l0bl@outlook.com>
2021-08-16 20:59:00 +0800

    build local css

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    minifyCss = require('gulp-clean-css'),
+    rename = require('gulp-rename');
+
+function clean() {
+    const deletedPaths = del.sync(['public/css/main.min.css']);
+    console.log('Deleted files:\n', deletedPaths.join('\n'));
+  };
+
+function cssMinify(){
+  gulp.src('public/css/main.css')
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/css'))
+ };
+
+ function build(cb){
+     clean();
+     cssMinify();
+     cb();
+ }
+
+exports.default = build;
